bf31f0be32128d318cb900058128caa2bcd84e77
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2023-04-18 23:19:13 +0100

    GroundCheck and Jumping
    - Jumping has been implemented for Waterform
    - Press up to jump
    - Press down after jumping to reset the jump mechanic
    - GroundChecker has been added and will be used to detect when Waterform has landed.

--- Assets/Scripts/Movement2.cs
+++ Assets/Scripts/Movement2.cs
@@ -6,56 +6,85 @@ public class Movement2 : MonoBehaviour
 {
     public float horizontalMove;
     public float verticalMove;
-    public float flareSpeed = 0f;
+    public float waterformSpeed = 0f;
     public float timer;
+    public bool isJumping = false;
+    public float yVel = 0f;
     public Rigidbody2D rb; //rigidbody
     public GameObject player;
+    public GameObject groundChecker;
+    public SortingLayer sortLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        flareSpeed = 5f;
+        waterformSpeed = 10f;
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //MOVE LEFT AND RIGHT
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             horizontalMove = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(horizontalMove * flareSpeed, verticalMove * flareSpeed/*rb.velocity.y*/);
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed/*rb.velocity.y*/);
             timer = 0.5f;
             Debug.Log("moving left?"); //register left!
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             horizontalMove = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(horizontalMove * flareSpeed, verticalMove * flareSpeed/*rb.velocity.y*/);
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed/*rb.velocity.y*/);
             timer = 0.5f;
             Debug.Log("moving right?"); //register left!
         }
-        else if (Input.GetKey(KeyCode.UpArrow)) //TO BE CHANGED//
+        else
         {
-            verticalMove = Input.GetAxis("Vertical");
-            rb.velocity = new Vector2(horizontalMove * flareSpeed, verticalMove * flareSpeed);
-            timer = 0.5f;
-            Debug.Log("moving up?"); //register left!
+            horizontalMove = 0;
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, /*verticalMove * yVel*/ 0); //should be = to 0,0
         }
-        else if (Input.GetKey(KeyCode.DownArrow)) //TO BE CHANGED//
+
+        //JUMPING//
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isJumping != true) //TO BE CHANGED//
         {
             verticalMove = Input.GetAxis("Vertical");
-            rb.velocity = new Vector2(horizontalMove * flareSpeed, verticalMove * flareSpeed);
+            //rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed);
+            isJumping = true;
+            yVel = 25f; //sets the height that it'll change by
+            timer = 0.5f;
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, yVel); //boing.
+            Debug.Log("pressed jump."); //register left!
+        }
+        //JUMPING IN THE AIR//
+        if (isJumping == true) //while in the air :D
+        {
+            if (yVel > -10f)
+            {
+                yVel = yVel - 0.1f; //decelerating in the air...
+            }
+            else if (yVel <= -10f)
+            {
+                yVel = -10f;
+                Debug.Log("max falling!"); //still falling...
+            }
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, yVel); //boing.
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) //TO BE CHANGED//
+        {
+            verticalMove = Input.GetAxis("Vertical");
+            isJumping = false; //for now.
+            yVel = 0f;
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed);
             timer = 0.5f;
             Debug.Log("moving down?"); //register left!
         }
-        else
-        {
-            horizontalMove = 0; //stop moving.
-            verticalMove = 0; //stop moving.
-            rb.velocity = new Vector2(horizontalMove * flareSpeed, verticalMove * flareSpeed); //should be = to 0,0
-        }
+        
+        
 
+        
 
         if (horizontalMove == 0 && verticalMove == 0)
         {
@@ -69,4 +98,17 @@ public class Movement2 : MonoBehaviour
 
         //TurnAroundBee(); not important atm
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log(collision.name); //what did you hit?
+
+
+
+    //    //private void DetectForGround()
+    //    //{
+    //    //    GetComponent(groundChecker);
+    //    //    Debug.Log(sortLayer)
+    //    //}
+    //}
 }
