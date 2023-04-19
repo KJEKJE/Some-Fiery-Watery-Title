1bcaf2c16be016004914766d6dd097daa9e029ee
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2023-04-19 03:00:15 +0100

    Charging fireballs
    - Made it so that fireballs can spawn larger the longer the space key is held
    - Adjusted the fireball prefab to be smaller (0.4 changed to 0.3)
    - Moved the spawner a bit further away from Flare for the time being.
    
    Would like to set it up with separate layers, but happy it functions atm ^^

--- Assets/Scripts/ShootFireball.cs
+++ Assets/Scripts/ShootFireball.cs
@@ -11,6 +11,9 @@ public class ShootFireball : MonoBehaviour
     public float xSin;
     public float ySin; //sohcahtoa at some point
     public float dir;
+
+    public float chargeTime; //how long flare holds a fireball for
+    public float multiplier; //how powerful the fireball becomes
     
     // Start is called before the first frame update
     void Start()
@@ -18,25 +21,56 @@ public class ShootFireball : MonoBehaviour
         launchSpeed = 5f; //initial launch speed
         dir = projectileSpawn.rotation.z; //for later?
         Debug.Log("Direction of projectile is: " + dir);
+        chargeTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)) //charges fireball
         {
-            Pew();
+            //while space is down, count time held and build up multiplier
+            chargeTime = chargeTime + Time.deltaTime;
+            //chargeTime++;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) 
+        {
+            Debug.Log("Power timer: " + chargeTime); //calculates fireball
+            
+            if (chargeTime <= 0.6f)
+            {
+                multiplier = 1f; //small
+            }
+            else if (chargeTime > 0.6f && chargeTime <= 1.0f)
+            {
+                multiplier = 2f; //medium
+            }
+            else if (chargeTime > 1f && chargeTime <= 1.5f)
+            {
+                multiplier = 3f; //large
+            }
+            else
+            {
+                multiplier = 4f; //dangerous
+            }
+
+            //when let go, deduce multiplier
+            Pew(); //releases fireball
+            chargeTime = 0;            
+            
         }
     }
 
     void Pew()
     {
+        Debug.Log("Multiplier: " + multiplier);
+        projectile.transform.localScale *= multiplier; //changes fireball size
         Instantiate(projectile, projectileSpawn.position, projectileSpawn.rotation);
+        projectile.transform.localScale /= multiplier; //resets it back to orignal size
+
+
         rb.velocity = new Vector2(launchSpeed, 0); //temporary launch
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-    }
+       
 }
