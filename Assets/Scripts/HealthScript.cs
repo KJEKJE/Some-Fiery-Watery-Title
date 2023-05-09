0ea770155c357dc5bf61143d30c58b6af2eb1f06
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2023-05-09 20:26:44 +0100

    Taking Damage
    - Players are set at 15 HP for now.
    - Can use numpad to 'take damage'
    - Players HP goes down once damage is taken.

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
+    public float playerHealth;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = 15f; //set it to a base 15 for now
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TakeDamage(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            TakeDamage(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            TakeDamage(3);
+        }
+    }
+
+    private void TakeDamage(int ouch)
+    {
+        Debug.Log("Took " + ouch + " damage.");
+        playerHealth = playerHealth +- ouch;
+        Debug.Log("Player now has " + playerHealth + " HP left.");
     }
 }
