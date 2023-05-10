b2e95c6ac8197bfcb7a3285a2a604b6fdb86c446
KJEKJE <55700917+KJEKJE@users.noreply.github.com>
2023-05-11 00:13:28 +0100

    FixedUpdate
    - Switched scripts around, updated jumping for Waterform to be able to jump consistently.

--- Assets/Scripts/MovementWaterform.cs
+++ Assets/Scripts/MovementWaterform.cs
@@ -8,6 +8,7 @@ public class MovementWaterform : MonoBehaviour
     public float verticalMove;
     public float waterformSpeed = 0f;
     public float timer;
+    private float maxTimer = 1f;
     public bool isJumping = false;
     public float yVel = 0f;
     public Rigidbody2D rb; //rigidbody
@@ -46,17 +47,32 @@ public class MovementWaterform : MonoBehaviour
             rb.velocity = new Vector2(horizontalMove * waterformSpeed, /*verticalMove * yVel*/ 0); //should be = to 0,0
         }
 
-        //JUMPING//
         if (Input.GetKeyDown(KeyCode.UpArrow) && isJumping != true) //TO BE CHANGED//
         {
             verticalMove = Input.GetAxis("Vertical");
             //rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed);
             isJumping = true;
             yVel = 5f; //sets the height that it'll change by
-            timer = 0.5f;
+            timer = 0; //trying again
             rb.velocity = new Vector2(horizontalMove * waterformSpeed, yVel); //boing.
             Debug.Log("pressed jump."); //register left!
         }
+
+        if (Input.GetKey(KeyCode.DownArrow)) //TO BE CHANGED//
+        {
+            verticalMove = Input.GetAxis("Vertical");
+            isJumping = false; //for now.
+            yVel = 0f;
+            rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed);
+            timer = 0.5f;
+            Debug.Log("moving down?"); //register left!
+        }
+    }
+
+        //JUMPING//
+     void FixedUpdate()
+     {
+        
         //JUMPING IN THE AIR//
         if (isJumping == true) //while in the air :D
         {
@@ -70,31 +86,23 @@ public class MovementWaterform : MonoBehaviour
                 Debug.Log("max falling!"); //still falling...
             }
             rb.velocity = new Vector2(horizontalMove * waterformSpeed, yVel); //boing.
+            timer += Time.deltaTime; //checks for how long
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)) //TO BE CHANGED//
-        {
-            verticalMove = Input.GetAxis("Vertical");
-            isJumping = false; //for now.
-            yVel = 0f;
-            rb.velocity = new Vector2(horizontalMove * waterformSpeed, verticalMove * waterformSpeed);
-            timer = 0.5f;
-            Debug.Log("moving down?"); //register left!
-        }
         
         
 
         
 
-        if (horizontalMove == 0 && verticalMove == 0)
-        {
-            //Debug.Log("stopped moving.");
-            timer -= Time.deltaTime;
-            if (timer <= 0f)
-            {
-                timer = 0.5f;
-            }
-        }
+        //if (horizontalMove == 0 && verticalMove == 0)
+        //{
+        //    //Debug.Log("stopped moving.");
+        //    timer -= Time.deltaTime;
+        //    if (timer <= 0f)
+        //    {
+        //        timer = 0.5f;
+        //    }
+        //}
 
         //TurnAround(); not important atm
     }
